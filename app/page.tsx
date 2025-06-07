dececf84642e8861a130b89623a6e38897655279
Shubham Bhat <bhatsupshubham@gmail.com>
2025-06-08 00:48:52 +0530

    deployment and tracking for vercel

--- app/page.tsx
+++ app/page.tsx
@@ -192,7 +192,7 @@ font-[Poppins] font-extrabold"
       </div>
       <footer className="footer footer-horizontal footer-center bg-base-200 text-base-content rounded p-10">
   <nav className="grid grid-flow-col gap-4">
-    <a className="link link-hover">About us</a>
+    <a href="" className="link link-hover">About us</a>
     <a className="link link-hover">Contact</a>
     <a className="link link-hover">Jobs</a>
     <a className="link link-hover">Press kit</a>
