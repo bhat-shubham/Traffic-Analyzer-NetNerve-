024b5781717b762cd0121f2834b5a30d847c7a73
Shubham Bhat <bhatsupshubham@gmail.com>
2025-06-19 14:49:51 +0530

    Remove backend/__pycache__ from git

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,11 @@
 from typing import Annotated
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI, File, UploadFile
-from scapy.all import rdpcap  # Importing rdpcap from scapy to read pcap files
+from scapy.all import rdpcap
+from scapy.layers.inet import IP, TCP, UDP, ICMP
+from scapy.layers.l2 import ARP
+
+
 app = FastAPI()
 
 app.add_middleware(
@@ -11,97 +15,62 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+def extract_packet_data(file_path):
+    packets = rdpcap(file_path)
+    data = []
 
-@app.post("/files/")
-async def create_file(file: Annotated[bytes, File()]):
-    return {"file_size": len(file)}
+    for pkt in packets:
+        pkt_info = {}
+
+        if IP in pkt:
+            pkt_info["src_ip"] = pkt[IP].src
+            pkt_info["dst_ip"] = pkt[IP].dst
+            pkt_info["packet_len"] = len(pkt)
+            pkt_info["timestamp"] = pkt.time
+
+            if TCP in pkt:
+                pkt_info["protocol"] = "TCP"
+                pkt_info["src_port"] = pkt[TCP].sport
+                pkt_info["dst_port"] = pkt[TCP].dport
+                pkt_info["flags"] = str(pkt[TCP].flags)
+
+            elif UDP in pkt:
+                pkt_info["protocol"] = "UDP"
+                pkt_info["src_port"] = pkt[UDP].sport
+                pkt_info["dst_port"] = pkt[UDP].dport
+
+            elif ICMP in pkt:
+                pkt_info["protocol"] = "ICMP"
+
+        elif ARP in pkt:
+            pkt_info["protocol"] = "ARP"
+
+        if pkt_info:
+            data.append(pkt_info)
+
+    return data
+
+# @app.post("/files/")
+# async def create_file(file: Annotated[bytes, File()]):
+#     return {"file_size": len(file)}
 
 
 @app.post("/uploadfile/")
+
 async def create_upload_file(file: UploadFile):
+    file_path= "temp.cap"
     content = await file.read();
-    with open("temp.cap", "wb") as f:
+    with open(file_path, "wb") as f:
         f.write(content)
-
-
-    a=rdpcap("temp.cap")
+    packets=rdpcap("temp.cap")
     protocols=set()
-    for packet in a:
+    for packet in packets:
         layer = packet
         while layer:
             protocols.add(layer.__class__.__name__)
             layer = layer.payload  # move to next inner layer
-
-    return {"protocols": list(protocols)}
-
-        # protocols.add(packet.name)
-        
-        # if packet.haslayer("IP"):
-        #     print(f"Source IP: {packet["IP"].src}, Destination IP: {packet["IP"].dst}")
-    return{"protocols": list(protocols)}
-    # content = await file.read();
-    # content = pyshark.FileCapture(UploadFile)
-    # content = await file.read()
-    # with open("temp.cap", "wb") as f:
-    #     f.write(content)
-    # with pyshark.FileCapture("temp.cap") as cap:
-    #     print("dd")
-    return{"ok"}
-
-    
-    # protocols = set()
-    # cap.close()
-    # for packet in cap:
-    #     cap[0]
-        # protocols.add(packet.highest_layer)
-    
-
-        
-    # cap = pyshark.FileCapture("temp.cap")
-    # return{cap[0]}
-# async def get_info(file: UploadFile):
-#     content = await file.read()
-#     with open("temp.cap", "wb") as f:
-#         f.write(content)
-    
-#     cap = pyshark.FileCapture("temp.cap")
-#     protocols = set()
-#     contentstr=""
-#     for packet in cap:
-#         protocols.add(packet.highest_layer)
-#         contentstr += str(packet.highest_layer) + "\n"
-#         cap.close()
-#     print ("cgc",contentstr)
-    
-#     # return {"protocols": list(protocols)}
-#     print("Protocols found:", protocols)
-    
-#     # return {"protocols": list(protocols)}
-            
-# return(content);
-
-
-
-# from fastapi import FastAPI, UploadFile, File
-# from fastapi.middleware.cors import CORSMiddleware
-# import pyshark
-
-# app=FastAPI()
-
-# app.add_middleware(
-#     CORSMiddleware,
-#     allow_origins=["http://localhost:3000"],
-#     allow_credentials=True,
-#     allow_methods=["*"],
-#     allow_headers=["*"],
-# )
-# @app.post("/upload/")
-# async def upload_file(file:UploadFile= File(...)):
-#     content= await file.read()
-#     with open("temp.cap","wb") as f:
-#         f.write(content)
-#     cap= pyshark.FileCapture("temp.cap")
-#     protocols=set()
-#     for packet in cap:
-#         protocols.add(packet.highest_layer)
-#     return {"protocols": list(protocols)}
+    packet_data = extract_packet_data(file_path)
+    return {
+        "protocols": list(protocols),
+        "packet_data": packet_data
+    }
